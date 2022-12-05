0933d416bf5fe9133dfaef12a4f2d1e57f05f314
hossein Rahmati <rmthossein@gmail.com>
2022-12-05 22:33:19 +0330

    get async todos on json-server

--- src/components/Todos/TodoList.js
+++ src/components/Todos/TodoList.js
@@ -1,9 +1,18 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getAsyncTodos } from "../../features/todos/todosSlice";
 import TodoItem from "./TodoItem";
 
 const TodoList = () => {
-  const { todos } = useSelector((state) => state.todos);
+  const { todos, error, loading } = useSelector((state) => state.todos);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getAsyncTodos());
+  }, []);
+
+  if (loading) return <p>loading...</p>;
+  if (error) return <p>{error}</p>;
 
   return (
     <ul className="list-group">
--- src/features/todos/todosSlice.js
+++ src/features/todos/todosSlice.js
@@ -1,18 +1,29 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+export const getAsyncTodos = createAsyncThunk(
+  "todos/getAsyncTodos",
+  async (_, { rejectWithValue }) => {
+    try {
+      const response = await axios.get("http://localhost:3001/todos");
+      return response.data;
+    } catch (error) {
+      return rejectWithValue([], error.message);
+    }
+  }
+);
 
 const initialState = {
-  todos: [
-    { id: 1, title: "todo1", completed: false },
-    { id: 2, title: "todo2", completed: false },
-    { id: 3, title: "todo3", completed: true },
-    { id: 4, title: "todo4", completed: false },
-    { id: 5, title: "todo5", completed: false },
-  ],
+  todos: [],
+  error: null,
+  loading: false,
 };
 
 const todosSlice = createSlice({
   name: "todos",
   initialState,
+
+  //this is for only sync actions, for async actions we should use extraReducers
   reducers: {
     addTodo: (state, action) => {
       // create an object
@@ -34,6 +45,17 @@ const todosSlice = createSlice({
         (t) => t.id !== action.payload.id
       );
       state.todos = filteredTodos;
+    },
+  },
+  extraReducers: {
+    [getAsyncTodos.fulfilled]: (state, action) => {
+      return { ...state, todos: action.payload, loading: false, error: null };
+    },
+    [getAsyncTodos.pending]: (state, action) => {
+      return { ...state, todos: [], loading: true, error: null };
+    },
+    [getAsyncTodos.rejected]: (state, action) => {
+      return { ...state, todos: [], loading: false, error: action.error };
     },
   },
 });
